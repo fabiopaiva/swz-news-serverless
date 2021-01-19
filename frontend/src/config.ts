fc8b3a09042f0d01605b6be26fe2ae2bae853f92
Fábio Paiva <fabio@paiva.info>
2021-01-19 23:25:00 +0100

    reb: url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,3 @@
-// export const config = {
-//   apiBaseUrl: "https://api.swz.paiva.info"
-// }
-
 export const config = {
-  apiBaseUrl: "https://nv0z6fyur7.execute-api.eu-central-1.amazonaws.com/prd"
+  apiBaseUrl: "https://api.swz.paiva.info"
 }
